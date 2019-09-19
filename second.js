53b0e8cb827b71d3e1e9f6564253993e3038acdc
Vadim Jets <35491433+OrionDreamer@users.noreply.github.com>
2019-09-19 16:39:10 +0300

    add new variable

--- second.js
+++ second.js
@@ -1,10 +1,26 @@
-const _ = require("lodash")
+// const _ = require("lodash")
 
-const words = ['sky', 'wood', 'forest', 'falcon', 
-    'pear', 'ocean', 'universe'];
+// const words = ['sky', 'wood', 'forest', 'falcon',
+//     'pear', 'ocean', 'universe'];
 
-let fel = _.first(words);
-let lel = _.last(words);
+// let fel = _.first(words);
+// let lel = _.last(words);
 
-console.log(`First element: ${fel}`);
-console.log(`Last element: ${lel}`);
+// console.log(`First element: ${fel}`);
+// console.log(`Last element: ${lel}`);
+
+const name = 'baad';
+let n = 1;
+
+console.log('hello');
+const jopa = balel => {
+  return balel;
+};
+console.log(jopa(name));
+const arr = [1, 2, 3, 4, 5];
+arr.forEach(num => {
+  console.log(num);
+});
+
+const female = 'superwoman';
+console.log('OUTPUT: female', female);
