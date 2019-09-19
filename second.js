97f977985d161c25b85a7530657fe856478b86c6
Vadim Jets <35491433+OrionDreamer@users.noreply.github.com>
2019-09-19 12:53:11 +0300

    traversy lessons

---
+++ second.js
@@ -0,0 +1,10 @@
+const _ = require("lodash")
+
+const words = ['sky', 'wood', 'forest', 'falcon', 
+    'pear', 'ocean', 'universe'];
+
+let fel = _.first(words);
+let lel = _.last(words);
+
+console.log(`First element: ${fel}`);
+console.log(`Last element: ${lel}`);
